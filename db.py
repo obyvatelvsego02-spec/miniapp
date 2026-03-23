0fe52e910ee30f9ce02519c8d38e561d01ee1dab
obyvatelvsego02-spec <obyvatel.vsego02@gmail.com>
2026-03-24 01:23:57 +1000

    Update db.py

--- db.py
+++ db.py
@@ -2,7 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL", "").strip()
+
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
